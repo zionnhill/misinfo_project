efc98c2fd7182d35c959ccdbf0ba78fbf2149dba
dj <104624695@student.swin.edu.au.com>
2025-11-09 23:45:59 +1100

    updated frontend

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SentimentChart from "../components/SentimentChart";
-import WordCloudChart from "../components/WordCloudChart";
 import "../styles/dashboard.css";
 
 const Dashboard = () => {
@@ -10,13 +9,7 @@ const Dashboard = () => {
     { name: "Negative", value: 60 },
   ];
 
-  const wordData = [
-    { text: "vaccine", value: 50 },
-    { text: "fake", value: 35 },
-    { text: "news", value: 30 },
-    { text: "health", value: 25 },
-    { text: "covid", value: 20 },
-  ];
+  
 
   return (
     <div className="dashboard-container">
@@ -25,9 +18,7 @@ const Dashboard = () => {
         <div className="chart-card">
           <SentimentChart data={sentimentData} />
         </div>
-        <div className="chart-card">
-          <WordCloudChart words={wordData} />
-        </div>
+        
       </div>
     </div>
   );
